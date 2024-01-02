447fa0ea9dc8f93019e26f90d6be452e7f9b3c3c
Ildar <ildarvagapov528@gmail.com>
2024-01-02 14:41:18 +0500

    fix lint

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -7,17 +7,14 @@ export enum ThemeButton {
     OUTLINE = 'outline',
 }
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>{
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     className?: string;
     theme?: ThemeButton;
 }
 
 export const Button: FC<ButtonProps> = (props) => {
     const {
-        className,
-        children,
-        theme,
-        ...otherProps
+        className, children, theme, ...otherProps
     } = props;
 
     return (
